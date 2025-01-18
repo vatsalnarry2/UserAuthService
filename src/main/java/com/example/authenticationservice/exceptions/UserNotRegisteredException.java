35d1c14a4f4708dd55a612e078ee6c38e99ccdd8
Naresh Vatsal <naresh_vatsal@thbs.com>
2025-01-18 13:25:03 +0530

    updated

--- src/main/java/com/example/authenticationservice/exceptions/UserNotRegisteredException.java
+++ src/main/java/com/example/authenticationservice/exceptions/UserNotRegisteredException.java
@@ -8,8 +8,7 @@ public class UserNotRegisteredException extends Exception{
 	private static final long serialVersionUID = -4801376122681507634L;
 	
 	public UserNotRegisteredException(String message) {
-		
-		super(message);
+				super(message);
 	}
 
 }
